004c137b14deaa7990fdf77af3eaf05320f751a9
panyu1512 <kikeferreragius1997@gmail.com>
2023-08-15 00:19:28 +0200

    create clear junk python script

---
+++ README.md
@@ -0,0 +1,63 @@
+# clearJunk - Python Script for Cleaning Up Directories
+
+The `clearJunk` Python script is designed to help you clean up and organize files within a specified directory. The script performs various cleaning and organizing tasks to help you manage your files more efficiently.
+
+## Table of Contents
+
+- [Introduction](#clearjunk---python-script-for-cleaning-up-directories)
+- [Table of Contents](#table-of-contents)
+- [Description](#description)
+- [Functions](#functions)
+  - [Initialization](#initialization)
+  - [`remove_temp_files(self)`](#remove_temp_filesself)
+  - [`remove_duplicated_files(self)`](#remove_duplicated_filesself)
+  - [`clean_up_directory(self)`](#clean_up_directoryself)
+  - [`remove_old_files(self)`](#remove_old_filesself)
+- [Usage](#usage)
+- [Important Notes](#important-notes)
+
+## Description
+
+The `clearJunk` script is organized into a class with several functions that perform different file management tasks:
+
+- `remove_temp_files(self)`: Removes temporary files with specific extensions from the directory.
+- `remove_duplicated_files(self)`: Identifies duplicated files with version numbers and keeps only the most recent version.
+- `clean_up_directory(self)`: Organizes files by moving them into subfolders based on their types and handles ZIP files.
+- `remove_old_files(self)`: Removes files that have been inactive for a specified number of days.
+
+## Functions
+
+### Initialization
+
+The `clearJunk` class is initialized with two parameters:
+
+- `directory_path`: The path to the directory you want to clean up.
+- `destination_path`: The path to the directory where organized files will be moved.
+
+### `remove_temp_files(self)`
+
+This function removes temporary files based on their extensions. It iterates through files in the specified directory and removes files with certain extensions (e.g., `.log`, `.cache`, `.tmp`, `.dmg`, `.pkg`).
+
+### `remove_duplicated_files(self)`
+
+This function identifies duplicated files in the specified directory and renames them to resolve the duplication. It searches for files with names in the format "name(number).extension" and keeps the most recent version of each file.
+
+### `clean_up_directory(self)`
+
+This function organizes files in the specified directory into subfolders based on their types. It iterates through the files and moves them to appropriate subfolders based on their extensions. If a ZIP file is encountered, its contents are extracted into a folder with the same name (minus `.zip`), and the ZIP file is removed.
+
+### `remove_old_files(self)`
+
+This function removes files that have been inactive for a specified number of days (`inactivity_days`). It calculates the last modified timestamp for each file and deletes files that haven't been modified within the specified time frame.
+
+## Usage
+
+1. Create an instance of the `clearJunk` class with the appropriate directory paths.
+2. Call the functions you want to execute. The script will perform tasks such as removing temporary files, organizing files by type, handling duplicated files, and removing old files based on your specifications.
+
+## Important Notes
+
+- Understand the script's behavior before running it on important data.
+- Modify extension lists and parameters to suit your specific requirements.
+- Check for dependencies in your environment (e.g., `os`, `shutil`, `re`, `zipfile`).
+- Backup your data before running scripts that modify or delete files.
---
+++ clearJunk.py
@@ -0,0 +1,172 @@
+
+import os
+import shutil
+import re
+import zipfile
+
+class clearJunk:
+    def __init__(self, directory_path, destination_path):
+        self.directory_path = os.path.expanduser(directory_path)
+        self.destination_path = os.path.expanduser(destination_path)
+    def remove_temp_files(self):
+        extensions = [
+            '.log',
+            '.cache',
+            '.tmp',
+            '.dmg',
+            '.pkg'
+        ]
+
+        for file in os.listdir(self.directory_path):
+            if os.path.splitext(file)[1] in extensions:
+                print(f"Removing {file}")
+                file_path = os.path.join(self.directory_path, file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.remove(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                    print(f"Successfully deleted {file_path}")
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+
+    def remove_duplicated_files(self):
+        for root, dirs, files in os.walk(self.directory_path):
+            file_dict = {}
+            for file in files:
+                file_path = os.path.join(root, file)
+                # check if file is a duplicate
+                match = re.search(r'^(.*?)\((\d+)\)(\.[^.]*)?$', file)
+                if match:
+                    name = match.group(1)
+                    number = int(match.group(2))
+                    extension = match.group(3)
+                    if name not in file_dict:
+                        file_dict[name] = [(number, file_path, extension)]
+                    else:
+                        file_dict[name].append((number, file_path, extension))
+            # Rename files in each group
+            for name in file_dict:
+                files = file_dict[name]
+                files.sort(key=lambda tup: tup[0])
+                print(files)
+                latest = files[-1][1]
+                extension = files[-1][2]
+                for number, file_path, _ in files[:-1]:
+                    print(f"Deleted duplicate file: {file_path}")
+                    os.remove(file_path)
+                os.rename(latest, os.path.join(root, f"{name}{extension}"))
+                print(f"Renamed {latest} to {name}{extension}")
+
+    def clean_up_directory(self):
+        file_types = {
+            "Image": [
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".gif",
+                ".bmp",
+                ".svg",
+                ".webp"
+            ],
+            "Video": [
+                ".mp4",
+                ".mov",
+                ".avi",
+                ".flv",
+                ".wmv",
+                ".mkv",
+                ".m4v",
+                ".mpeg",
+                ".mpg",
+                ".3gp"
+            ],
+            "Audio": [
+                ".mp3",
+                ".wav",
+                ".aac",
+                ".ogg",
+                ".flac",
+                ".wma",
+                ".alac"
+            ],
+            "Document": [
+                ".pdf",
+                ".doc",
+                ".docx",
+                ".xls",
+                ".xlsx",
+                ".ppt",
+                ".pptx",
+                ".txt",
+                ".rtf",
+                ".tex",
+                ".pages",
+                ".numbers",
+                ".key"
+            ],
+            "Archive": [
+                ".zip",
+                ".tar",
+                ".gz",
+                ".7z",
+                ".rar",
+                ".pkg",
+                ".dmg",
+                ".iso"
+            ],
+            "Code": [
+                ".py",
+                ".c",
+                ".cpp",
+                ".java",
+                ".class",
+                ".cs",
+                ".h",
+                ".sh",
+                ".swift",
+                ".vb",
+                ".js",
+                ".ts",
+                ".css",
+                ".html",
+                ".xml",
+                ".json"
+            ],
+        }
+
+        # Create folders for each file type
+        for folder_name in file_types.keys():
+            folder_path = os.path.join(self.destination_path, folder_name)
+            if not os.path.isdir(folder_path):
+                os.makedirs(folder_path)
+        # Iterate over the files in the directory
+        for file in os.listdir(self.directory_path):
+            file_path = os.path.join(self.directory_path, file)
+            # unzipped files and delete the zip file
+            if file.endswith(".zip"):
+                unzip_directory = os.path.join(self.directory_path, file[:-4])
+                if not os.path.exists(unzip_directory):
+                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                        zip_ref.extractall(self.directory_path)
+                # Delete the zip file
+                os.remove(file_path)
+                print(f"Unzipped {file_path} and removed {file}")
+
+            # Move files to their respective folders
+            else:
+                for folder_name, extensions in file_types.items():
+                    if os.path.splitext(file)[1] in extensions:
+                        destination_folder_path = os.path.join(self.destination_path, folder_name)
+                        shutil.move(file_path, os.path.join(destination_folder_path, file))
+                        print(f"Moved {file_path} to {destination_folder_path}")
+                        break
+
+    def remove_old_files(self):
+        inactivity_days = 90
+        for root, dirs, files in os.walk(self.directory_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.stat(file_path).st_mtime < inactivity_days * 86400:
+                    os.remove(file_path)
+                    print(f"Removed {file_path}")
